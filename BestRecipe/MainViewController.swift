324a282206597c5de8774664af72c31bdc9c5678
Zarina Sadykova <z.sadikova@mercuryo.io>
2025-08-17 16:56:34 +0500

    Changed background color in MainViewController

--- BestRecipe/MainViewController.swift
+++ BestRecipe/MainViewController.swift
@@ -11,7 +11,7 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemYellow
     }
 
 
