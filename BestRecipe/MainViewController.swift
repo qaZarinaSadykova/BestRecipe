63a3147a974ce6053003f69c6a0e716e37dce760
Zarina Sadykova <z.sadikova@mercuryo.io>
2025-08-17 16:53:44 +0500

    Initial commit

---
+++ BestRecipe/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestRecipe
+//
+//  Created by iMacbook on 8/15/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
